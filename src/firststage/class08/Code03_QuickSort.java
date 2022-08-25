250b4161f8858e8aa2324fe867e79e71956af640
helisi233 <hls12343@163.com>
2022-08-25 23:28:50 +0800

    second

--- src/firststage/class08/Code03_QuickSort.java
+++ src/firststage/class08/Code03_QuickSort.java
@@ -10,31 +10,55 @@ public class Code03_QuickSort {
         array[second] = temp;
     }
 
-    public static int partition(int[] array, int l, int r) {
-        int pivot = array[r];
-        int partition = l-1;
-        for (int i = l; i < r; i++) {
-            if(array[i] < pivot) {
-                swap(array, i, partition++);
+    public static int partition(int[] array, int start, int end) {
+        int pivot = array[end];
+        int partition = -1;
+        for (int index = start; index < end; index++) {
+            if(array[index] <= pivot) {
+                swap(array, index, ++partition);
             }
         }
-        swap(array, r, partition);
+        swap(array, ++partition, end);
         return partition;
     }
 
-    public static void quickSort(int[] array, int l, int r) {
+    public static int[] partition2(int[] array, int start, int end) {
+        int index = 0;
+        int less = -1;
+        int more = end;
+
+        while(index < more) {
+            if(array[index] < array[end]) {
+                swap(array, index++, ++less);
+            }else if(array[index] > array[end]) {
+                swap(array, index, --more);
+            }else {
+                index ++;
+            }
+        }
+        swap(array, more, end);
+        return new int[]{less, more};
+    }
+
+    public static void process(int[] array, int start, int end) {
+        if(start >= end) {
+            return;
+        }
+        int[] result = partition2(array, start, end);
+        process(array, start, result[0]);
+        process(array, result[1], end);
+    }
+
+    public static void quickSort(int[] array) {
         if(array == null || array.length < 2) {
             return;
         }
-        if(l > r) return;
-        int partition = partition(array, l, r);
-        quickSort(array, l, partition-1);
-        quickSort(array, partition+1, r);
+        process(array, 0, array.length-1);
     }
 
     public static void main(String[] args) {
         int[] randomArray = ArrayUtils.generateRandomArray(20, 100);
-        quickSort(randomArray, 0, randomArray.length-1);
+        quickSort(randomArray);
         ArrayUtils.printlnArray(randomArray);
     }
 }
